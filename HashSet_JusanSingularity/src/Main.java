784b3ce528f1e25a3d1cc218a05116ffe0f3874b
EldarToleubay <49515746+EldarToleubay@users.noreply.github.com>
2022-03-24 13:47:09 +0600

    ss

--- HashSet_JusanSingularity/src/Main.java
+++ HashSet_JusanSingularity/src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+        int one;
+        int ten;
+        int hundred;
+        if (x<0){
+            x=Math.abs(x);
+            one = ((x%100)%10);
+            ten = ((x%100)-one);
+            hundred = x - ten - one;
+            one*=100;
+            hundred/=100;
+            System.out.println(-(hundred+one+ten));
+        }else {
+            one = ((x%100)%10);
+            ten = ((x%100)-one);
+            hundred = x - ten - one;
+            one*=100;
+            hundred/=100;
+            System.out.println(hundred+one+ten);
+        }
     }
 }
