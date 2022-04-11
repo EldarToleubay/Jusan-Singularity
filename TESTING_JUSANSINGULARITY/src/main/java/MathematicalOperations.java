d24c7e819b381407e8c03d9a700b6172404f1942
Eldar Toleubay <eldartoleubay@gmail.com>
2022-04-11 09:50:47 +0600

    first Maven

---
+++ TESTING_JUSANSINGULARITY/src/main/java/MathematicalOperations.java
@@ -0,0 +1,10 @@
+public class MathematicalOperations {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1,2));
+    }
+}
